c4961793bd53013d8b56a461e950655c6c2f232e
Franko Jolić <franko.jolic@cml.fraunhofer.de>
2025-09-01 00:22:34 +0200

    changing folder structure

--- simulation/data/get_drive_plan.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-url = "https://www.tricon-terminal.de/info-desk/fahrplan"
-dfs = pd.read_html(url)
-
-print(len(dfs))
-
-dfs[0].to_csv('driving_plan_versand.csv', index=False)
-dfs[2].to_csv('driving_plan_empfang.csv', index=False)
