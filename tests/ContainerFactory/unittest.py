5562eaa1ae16590435ff6bc6f4374140707c9e92
Franko Jolić <franko.jolic@cml.fraunhofer.de>
2025-09-07 14:45:04 +0200

    updating dependancy

--- tests/ContainerFactory/unittest.py
+++ tests/ContainerFactory/unittest.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from simulation.terminal_components.storage_units.Container import Container
-from simulation.terminal_components.systems.ContainerFactory import ContainerFactory
+from simulation.terminal_components.storage_units.ContainerFactory import ContainerFactory
 
 
 class TestContainerFactory(unittest.TestCase):
